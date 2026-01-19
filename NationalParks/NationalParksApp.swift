b8d108a39ddfdc10b1c4757f2be8b82e83dc58cc
Martin <marrty.h.1992@gmail.com>
2026-01-19 19:25:08 +0100

    Initial Commit

---
+++ NationalParks/NationalParksApp.swift
@@ -0,0 +1,17 @@
+//
+//  NationalParksApp.swift
+//  NationalParks
+//
+//  Created by Martin Hrbáček on 19.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct NationalParksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
